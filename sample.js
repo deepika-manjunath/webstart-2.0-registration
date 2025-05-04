23d87cdf83bae553b3be9f8b693aa2693ea11efd
deepika-manjunath <deepikanela@gmail.com>
2025-05-04 20:25:37 +0530

    2nd commit

---
+++ sample.js
@@ -0,0 +1,37 @@
+new FinisherHeader({
+    "count": 5,
+    "size": {
+      "min": 2,
+      "max": 350,
+      "pulse": 0
+    },
+    "speed": {
+      "x": {
+        "min": 0,
+        "max": 0.8
+      },
+      "y": {
+        "min": 0,
+        "max": 0.2
+      }
+    },
+    "colors": {
+      "background": "#000000",
+      "particles": [
+        "#afff6b",
+        "#3daf2c",
+        "#2acb2b",
+        "#70e064",
+        "#a5feb4"
+      ]
+    },
+    "blending": "overlay",
+    "opacity": {
+      "center": 0.4,
+      "edge": 1
+    },
+    "skew": 0,
+    "shapes": [
+      "c"
+    ]
+  });
